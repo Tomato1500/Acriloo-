88eb76efe030a9ed444698dbfd7b7976d555cca9
Tomato1500 <Tomato1500@hotmail.com>
2025-04-27 22:40:46 +0900

    v0.1
    ウィンドウの外観を変更し、ボタン機能を削除
    
    MainWindow.xaml で DesktopAcrylicBackdrop を追加しました。これにより、ウィンドウにアクリル効果が適用されます。

--- Acriloo!/MainWindow.xaml.cs
+++ Acriloo!/MainWindow.xaml.cs
@@ -28,9 +28,5 @@ namespace Acriloo_
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
     }
 }
